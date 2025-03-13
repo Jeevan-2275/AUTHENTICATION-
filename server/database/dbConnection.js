283161b538d2dfd6cb479528ebcf99c24ca887c7
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-03-13 17:36:36 +0530

    🛢️ Enhanced MongoDB connection handling in dbConnection.js

---
+++ server/database/dbConnection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connection = async () => {
+    mongoose.connect(process.env.MONGO_URI , {
+dbName:"MERN_AUTHENTICATION"
+    }).then(()=>{
+        console.log("Connected to Database .")
+    }).catch(err=>{
+        console.log(`Some error occured while connecting to database: ${err}`);
+    });
+};
