26637b49b136a57dded95ad90b0681c93e672963
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-03-13 17:42:13 +0530

    🛡️ Updated middleware functions for improved security and performance

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -23,15 +23,15 @@ export const errorMiddleware = (err, req, res, next) => {
       err = new ErrorHandler(message, 400);
     }
   
-//     if (err.code === 11000) {
-//       const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
-//       err = new ErrorHandler(message, 400);
-//     }
-// //   
-//     // return res.status(err.statusCode).json({
-//     //   success: false,
-//     //   message: err.message,
-//     // });
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+      err = new ErrorHandler(message, 400);
+    }
+//   
+    // return res.status(err.statusCode).json({
+    //   success: false,
+    //   message: err.message,
+    // });
   };
   
   export default ErrorHandler;
