ede8297179b4bd9a1c6918f843689f0ab5c75ff4
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-03-13 17:36:26 +0530

    🛢️ Integrated database connection in app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-// import { connection } from "./database/dbConnection.js";
+import { connection } from "./database/dbConnection.js";
 // import { errorMiddleware } from "./middlewares/error.js";
 // import userRouter from "./routes/userRouter.js";
 // import { removeUnverifiedAccounts } from "./automation/removeUnverifiedAccounts.js";
@@ -25,6 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.use("/api/v1/user", userRouter);
 
 // removeUnverifiedAccounts();
-// connection();
+connection();
 
 // app.use(errorMiddleware);
