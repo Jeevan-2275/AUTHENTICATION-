c6389acadf5562c70ee6568f506fc8ad286b73ae
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-03-13 18:04:22 +0530

    Refactored and optimized Mongoose models for better database management

---
+++ server/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import crypto from "crypto";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: {
+    type: String,
+    minLength: [8, "Password must have at least 8 characters."],
+    maxLength: [32, "Password cannot have more than 32 characters."],
+    select: false,
+  },
+  phone: String,
+  accountVerified: { type: Boolean, default: false },
+  verificationCode: Number,
+  verificationCodeExpire: Date,
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+ 
