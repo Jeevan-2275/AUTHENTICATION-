16ca4b7fa4f36e1831747e709a5765bab55b1d26
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-03-16 22:12:56 +0530

    🔗 Improved database connection handling in dbConnection.js

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
 export const connection = async () => {
-    mongoose.connect(process.env.MONGO_URI , {
-dbName:"MERN_AUTHENTICATION"
-    }).then(()=>{
-        console.log("Connected to Database .")
-    }).catch(err=>{
-        console.log(`Some error occured while connecting to database: ${err}`);
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_AUTHENTICATION",
+    })
+    .then(() => {
+      console.log("Connected to Database .");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database: ${err}`);
     });
 };
